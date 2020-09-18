92b33a0afe3d43cce83bdc6578c980890e380711
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2020-09-18 17:13:04 +0200

    Exercise part A done

--- Project1/part_a.py
+++ Project1/part_a.py
@@ -1,74 +1,61 @@
 # FYS-STK4155 - H2020 - UiO
-# Project 1
+# Project 1 - part a
 # Author: Fábio Rodrigues Pereira
 # E-mail: fabior@uio.no
 
 
-from HelperFunctions import create_data
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from LinearRegression import LinearRegressionTechniques
-from Metrics import r2, mse
+from package.Create_data import CreateData
+from package.Pre_processing import PreProcessing
+from package.LinearRegression import LinearRegressionTechniques
+from package.Pipeline import MakePipeline
+from sklearn.metrics import r2_score, mean_squared_error
 
+# initializing a pipeline
+pipe = MakePipeline(
+    create_data_model=CreateData(),
+    pre_processing_model=PreProcessing(),
+    ml_model=LinearRegressionTechniques(technique_name="OLS")
+)
 
-def part_a(nr_samples, poly_degrees, seed):
-    for poly_degree in poly_degrees:
-        X, z = create_data(nr_samples, poly_degree, seed)
+for degree in [2, 3, 4, 5]:
 
-        # splitting the data in test and training data
-        X_train, X_test, z_train, z_test = \
-            train_test_split(X, z, test_size=0.2)
+    # setting parameters to create the data
+    pipe.set_data_model_new_parameters(
+        model_name="FrankeFunction",
+        nr_samples=100,
+        degree=degree,
+        seed=10
+    )
 
-        # scaling X_train and X_test
-        scaler = StandardScaler().fit(X_train)
-        X_train_scaled = scaler.transform(X_train)
-        X_test_scaled = scaler.transform(X_test)
+    # setting pre-processing parameters
+    pipe.set_pre_processing_new_parameters(
+        test_size=0.25,
+        seed=10,
+        split=True,
+        scale=True
+    )
 
-        # del unnecessary objects stored
-        # performance purposes
-        del X, z, X_test, X_train, scaler
+    # fitting and predicting values for [2, 3, 4, 5] degrees
+    z_predict_train, z_predict_test = pipe.fit_predict()
 
-        # finding betas by OLS
-        # call the LinearRegressionTechniques
-        # and fit data in the OLS model
-        lr = LinearRegressionTechniques(technique_name="OLS")
-        lr.fit(X_train_scaled, z_train)
+    # print coefficients beta
+    print("Betas for polynomial of {} degree: ".format(degree),
+          pipe.get_coeffs(), "\n")
 
-        # getting coefficients (betas)
-        print("Beta_hat :", lr.coef_OLS)
+    # print confidence interval for betas
+    print("CI for the betas: ", pipe.get_CI_coeffs_(), "\n")
 
-        # accessing the confidence interval of the coefficients
-        # significance level = 95%
-        print("CI: ", lr.coef_CI_OLS)
+    # getting necessary parameters
+    z_train, z_test = pipe.get_z_values()
 
-        # predicting the response variable
-        # and accessing the accuracies
-        z_train_predict = lr.y_predict
-        print("Training R2: ", r2(z_train, z_train_predict))
-        print("Training MSE: ", mse(z_train, z_train_predict))
+    # evaluating training MSE and R2-score
+    print("Training MSE : ",
+          mean_squared_error(z_train, z_predict_train))
+    print("Training R2-score : ",
+          r2_score(z_train, z_predict_train), "\n")
 
-        z_test_predict = lr.predict(X_test_scaled)
-        print("Testing R2: ", r2(z_test, z_test_predict))
-        print("Testing MSE: ", mse(z_test, z_test_predict))
-
-        # # # # # # # # # # # # # # # # SKLEARN - Linear Regression
-        from sklearn.linear_model import LinearRegression
-        from sklearn.metrics import r2_score, mean_squared_error
-
-        lr1 = LinearRegression()
-        lr1.fit(X_train_scaled, z_train)
-
-        print("###### SKLEARN")
-        print("Beta_hat: ", lr1.coef_)
-
-        z_hat = lr1.predict(X_train_scaled)
-        print("R2 score: ", r2_score(z_train, z_hat))
-        print("MSE: ", mean_squared_error(z_train, z_hat))
-
-        z_tilde = lr1.predict(X_test_scaled)
-        print("R2 score: ", r2_score(z_test, z_tilde))
-        print("MSE: ", mean_squared_error(z_test, z_tilde))
-
-
-if __name__ == "__main__":
-    part_a(nr_samples=5, poly_degrees=[2], seed=10)
+    # evaluating test MSE and R2-score
+    print("Test MSE : ",
+          mean_squared_error(z_train, z_predict_train))
+    print("Test R2-score : ",
+          r2_score(z_train, z_predict_train), "\n")
