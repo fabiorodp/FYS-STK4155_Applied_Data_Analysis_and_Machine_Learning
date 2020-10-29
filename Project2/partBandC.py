9ed8a891fd58089e583ed30a73cb4a3dee61745e
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2020-10-29 19:33:24 +0100

    Exercise in progress

---
+++ Project2/partBandC.py
@@ -0,0 +1,12 @@
+# UiO: FYS-STK4155 - H20
+# Project 2
+# Author: Fabio Rodrigues Pereira
+# E-mail: fabior@uio.no
+
+"""
+partBandC.py
+~~~~~~~~~~
+
+A script to perform the exercise Part B and C of Project 2.
+"""
+
