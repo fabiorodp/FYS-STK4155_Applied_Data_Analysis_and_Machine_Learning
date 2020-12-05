64fbef4cb44357603454089336a20e1f29c7d211
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2020-12-04 21:19:42 -0300

    Handling data simplified

---
+++ Project3/handling_data.py
@@ -0,0 +1,32 @@
+# UiO: FYS-STK4155 - H20
+# Project 3
+# Author: Fabio Rodrigues Pereira
+# E-mail: fabior@uio.no
+
+import numpy as np
+import pandas as pd
+from Project3.package.read_data import get_instrument, create_candles
+
+
+# function to separate all negotiations of a single ticker from all raw data
+get_instrument(
+    ticker='PETR4',
+    in_folder='Project3/data/raw/',
+    out_folder='Project3/data/PETR4/'
+)
+
+# generating data in a given periodicity=1d
+create_candles(
+    ticker='PETR4',
+    candles_periodicity='1D',
+    in_folder='Project3/data/PETR4/',
+    out_folder='Project3/data/'
+)
+
+# generating data in a given periodicity=1d
+create_candles(
+    ticker='PETR4',
+    candles_periodicity='15min',
+    in_folder='Project3/data/PETR4/',
+    out_folder='Project3/data/'
+)
